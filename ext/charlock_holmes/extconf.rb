8d99ac8584f5c7896805345641ad447f3307fbdc
Jeremy Redburn <jredburn@salsify.com>
2013-03-05 18:30:00 -0500

    Revert "Allow use of ICU_DIR to specify icu installation"
    This reverts commit df63c181d005f8841ffd527fcd0fc82ead7f696b.

--- ext/charlock_holmes/extconf.rb
+++ ext/charlock_holmes/extconf.rb
@@ -23,29 +23,27 @@ end
 
 dir_config 'icu'
 
-icu4c = ENV['ICU_DIR'] if ENV['ICU_DIR']
-
 # detect homebrew installs
-if !icu4c && !have_library 'icui18n'
+if !have_library 'icui18n'
   base = if !`which brew`.empty?
     `brew --prefix`.strip
   elsif File.exists?("/usr/local/Cellar/icu4c")
     '/usr/local/Cellar'
   end
 
-  icu4c = Dir[File.join(base, 'Cellar/icu4c/*')].sort.last if base
+  if base and icu4c = Dir[File.join(base, 'Cellar/icu4c/*')].sort.last
+    $INCFLAGS << " -I#{icu4c}/include "
+    $LDFLAGS  << " -L#{icu4c}/lib "
+  end
 end
 
-unless !icu4c or (have_library 'icui18n' and have_header 'unicode/ucnv.h')
+unless have_library 'icui18n' and have_header 'unicode/ucnv.h'
   STDERR.puts "\n\n"
   STDERR.puts "***************************************************************************************"
   STDERR.puts "*********** icu required (brew install icu4c or apt-get install libicu-dev) ***********"
   STDERR.puts "***************************************************************************************"
   exit(1)
 end
-
-$INCFLAGS << " -I#{icu4c}/include "
-$LDFLAGS  << " -L#{icu4c}/lib "
 
 ##
 # libmagic dependency
